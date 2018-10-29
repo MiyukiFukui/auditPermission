80a9ef4f70c46fab23f53610002539b0b6c756cd
fukui-miyuki <co_miyuki-fukui@c-fo.com>
2018-10-29 17:28:34 +0900

    SettingSheetからSettingに名前を変えた

--- Class/SettingSheet.js
+++ Class/SettingSheet.js
@@ -1,14 +1,16 @@
-function SettingSheet(){
+function Setting(){
   this.id  = "";
   this.file = SpreadsheetApp.openById(this.id);
   this.sheet = this.file.getSheetByName("setting");
 
   //TODO:今ここでまとめてやってしまっているけど、分けたほうが良い？
+  this.csvUrl = this.getSetting("B5");
+
   this.includeFolder = this.getSetting("B2");
   this.filterDayAgo = this.getSetting("B3");
   this.replacePermission = this.getSetting("B4");
 }
 
-SettingSheet.prototype.getSetting = function(cell){
+Setting.prototype.getSetting = function(cell){
   return this.sheet.getRange(cell).getValue();
 }
