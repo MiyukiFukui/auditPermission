76ba59fc83326187e128cb6344d1e870f6295f9c
fukui-miyuki <co_miyuki-fukui@c-fo.com>
2018-10-29 17:31:07 +0900

    requestBodyの(methodやpayload部分以外の)中身を作る処理を追加した。

--- Class/OAuthService.js
+++ Class/OAuthService.js
@@ -1,14 +1,37 @@
-function OAuthService(option){
+
+//TODO:現在、他のプロジェクトのプロパティ情報を使用している状態で書いているので、修正しておく事
+var AdminProperty = Admin.getAllProperty();
+var serviceAccount = JSON.parse(AdminProperty.getProperty("jsonkey"));
+
+var googleDriveOpt = {
+    Service : "GoogleDrive",
+    TokenUrl : "https://accounts.google.com/o/oauth2/token",
+    PrivateKey : serviceAccount.private_key,
+    Issuer : serviceAccount.client_email,
+    PropertyStore : PropertiesService.getScriptProperties(),
+    Param : {
+      access_type : "offline",
+      approval_prompt : "force"
+    },
+    Scope : "https://www.googleapis.com/auth/drive"
+  }
+
+function OAuthService(userEmail){
+  var option = googleDriveOpt;
+  
   this.service = OAuth2.createService(option.Service)
   .setTokenUrl(option.TokenUrl)
   .setPrivateKey(option.PrivateKey)
   .setIssuer(option.Issuer)
   .setTokenUrl(option.TokenUrl)
+  .setSubject(userEmail)
   .setPropertyStore(option.PropertyStore)
   .setParam(option.Param)
   .setScope(option.Scope);
   return this;
 }
+
+
 
 OAuthService.prototype.run = function(url, requestBody){
   var service = this.service;
@@ -33,3 +56,12 @@ OAuthService.prototype.run = function(url, requestBody){
     console.log('認証情報の効果が切れています');
   }
 }
+
+OAuthService.prototype.createRequestBody = function(){
+  var requestBody = {};
+  requestBody.headers = { Authorization: 'Bearer ' + this.service.getAccessToken() };
+  requestBody.contentType = 'application/json';
+  requestBody.muteHttpExceptions = true;
+  return requestBody;
+}
+
